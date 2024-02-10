ed2a9cffd25c13c0735b4096b1ecde6d3ec6d871
Angatia Benson <angatiabenson1@gmail.con>
2024-02-10 09:45:35 -0600

    feat: Added destroy route and method to TaskController.

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -35,4 +35,10 @@ class TaskController extends Controller
         $task->update($validated);
         return new TaskResource($task);
     }
+
+    public function destroy(Request $request, Task $task)
+    {
+        $task->delete();
+        return response()->noContent();
+    }
 }
